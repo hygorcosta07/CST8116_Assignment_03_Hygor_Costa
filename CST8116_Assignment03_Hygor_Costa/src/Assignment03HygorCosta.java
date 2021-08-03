957f7a42d5dc2acb3ed87e7caa291d5531c4ae39
Hygor Costa <cost0142@algonquinlive.com>
2021-08-03 00:50:25 -0300

    Added programmer comments, and starter code to method main.

--- CST8116_Assignment03_Hygor_Costa/src/Assignment03HygorCosta.java
+++ CST8116_Assignment03_Hygor_Costa/src/Assignment03HygorCosta.java
@@ -3,7 +3,26 @@ public class Assignment03HygorCosta {
 
 	public static void main(String[] args) {
 
-System.out.println("teste");
+		// Declarations and initializing them and declaring the length of the Array
+		int roll = 0;
+		int[] diceRolls = new int[20];
+		int totalRolls = 0;
+
+		// Rules for Randometer and maximum and minimum parameters for it.
+		for (int count = 0; count < 1000; count++) {
+			roll = (int) (Math.random() * 20) + 1;
+			diceRolls[roll] = diceRolls[roll - 1] + 1;
+		}
+
+		// Report creation configuration.
+		for (int index = 0; index < diceRolls.length; index++) {
+			System.out.printf("Count of %d is: %d%n", (index + 1), diceRolls[index]);
+			totalRolls = totalRolls + diceRolls[index];
+		}
+		// output
+		System.out.println("Total rolls were: " + totalRolls);
+		System.out.println("Program by Hygor Costa");
+
 	}
 
 }
